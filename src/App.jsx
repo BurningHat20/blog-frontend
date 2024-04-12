dbf99cf3ea4518d544f6eaf1eb47658f1a919ca2
BurningHat12 <yashgohel395@gmail.com>
2024-04-12 19:11:16 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import LandingPage from "./components/LandingPage";
+import LandingNav from "./components/LandingNav";
+
+function App() {
+  return (
+    <div>
+      <LandingNav />
+      <LandingPage />
+    </div>
+  );
+}
+
+export default App;
